dd955f64528e7c273be592018d927e26cdf06b84
gbcruz <143095112+gbcruz@users.noreply.github.com>
2025-11-17 09:53:25 -0300

    vini changes

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,6 +1,15 @@
 export const ROUTES = {
     CADASTRO_ALUNO: '/cadastroAluno',
     CADASTRO_PROFESSOR: '/cadastroProfessor',
-    MATERIAS_ALUNO: '/materiasAluno',
     MATERIAS_PROFESSOR: '/materiasProf',
+<<<<<<< Updated upstream
+=======
+    TELA_PROFESSOR01: '/telaProfessor01',
+    TELA_PROFESSOR02: '/telaProfessor02',
+    TELA_PROFESSOR03: '/telaProfessor03',
+    TELA_PROFESSOR04: '/telaProfessor04',
+    MATERIAS_ALUNO: '/telaMaterias',
+    ATIVIDADES_ALUNO: '/telaAtividades',
+    QUESTOES_ALUNO: '/telaQuestoes',
+>>>>>>> Stashed changes
 } as const;
